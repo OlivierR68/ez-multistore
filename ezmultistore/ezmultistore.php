b9d0dc0b66b308ccaa3bd60ae35c821b32fdd33d
OlivierR68 <57542189+OlivierR68@users.noreply.github.com>
2020-04-08 11:45:52 +0200

    BugFix : Création de transporteur

--- ezmultistore/ezmultistore.php
+++ ezmultistore/ezmultistore.php
@@ -26,19 +26,54 @@ class EzMultiStore extends Module
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
-        if (!Configuration::get('MYMODULE_NAME')) {
-            $this->warning = $this->l('No name provided');
-        }
 
     }
 
-    public function install()
-    {
-        return parent::install();
+    public function install() {
+
+        if (!parent::install()
+            || !$this->_newCarrier()
+        ){
+            return false;
+        }
+        return true;
     }
 
     public function uninstall()
     {
         return parent::uninstall();
+    }
+
+    private function _newCarrier()
+    {
+        $id_lang = $id_lang = $this->context->language->id;
+
+        // Tableau pour tester, à remplacer par une table bdd
+        $stores = [
+            'Colmar' => [
+                'name' => 'Colmar : pick up in store',
+                'delay' => '2 hours',
+          ],
+            'Mulhouse' => [
+                'name' => 'Mulhouse : pick up in store',
+                'delay' => '2 hours',
+            ],
+            'Strasbourg' => [
+                'name' => 'Strasbourg : pick up in store',
+                'delay' => '2 hours',
+            ]
+        ];
+
+        foreach ($stores as $store) {
+            $carrier = new Carrier();
+
+            $carrier->name = $store['name'];
+            $carrier->delay[$id_lang] = $store['delay'];
+            $carrier->is_free = true;
+
+            $carrier->save();
+        }
+
+        return true;
     }
 }
