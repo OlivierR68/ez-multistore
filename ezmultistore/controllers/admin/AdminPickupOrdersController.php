47f99c6c55b672d27fcc969d366dc79322ef9ec9
OlivierR68 <57542189+OlivierR68@users.noreply.github.com>
2020-04-10 18:27:20 +0200

    Create AdminPickupOrdersController.php

---
+++ ezmultistore/controllers/admin/AdminPickupOrdersController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class AdminPickupOrdersController extends ModuleAdminController
+{
+    public function __construct()
+    {
+        $this->lang = false;
+        parent::__construct();
+    }
+
+    /**
+    * Method Translation Override For PS 1.7
+    */
+    public function l($string, $class = null, $addslashes = false, $htmlentities = true)
+    {
+        if (method_exists('Context', 'getTranslator')) {
+            $this->translator = Context::getContext()->getTranslator();
+            $translated = $this->translator->trans($string);
+
+            if ($translated !== $string) {
+                return $translated;
+            }
+        }
+        if ($class === null || $class == 'AdminTab') {
+            $class = Tools::substr(get_class($this), 0, -10);
+        } elseif (Tools::strtolower(Tools::substr($class, -10)) == 'controller') {
+            $class = Tools::substr($class, 0, -10);
+        }
+        return Translate::getAdminTranslation($string, $class, $addslashes, $htmlentities);
+    }
+}
+
+
